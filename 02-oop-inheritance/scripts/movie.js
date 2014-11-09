9041acfcb98cfd0942ac0e3395864cbb96ff6fb8
wenceslao01 <whoznogueira@gmail.com>
2014-11-09 03:45:09 -0300

    topic 2 corrections

--- 02-oop-inheritance/scripts/movie.js
+++ 02-oop-inheritance/scripts/movie.js
@@ -7,7 +7,6 @@ function Movie(){
 
 
 Movie.prototype = {
-	constructor:Movie,
 	play:function(){
 		$.publish("playing",[this.get("title")]);
 	},
@@ -17,7 +16,7 @@ Movie.prototype = {
 	set:function(attr,value){
 		switch (attr){											//this switch is used to control if the attribute to set is valid//
 		case "title": this.attributes[attr]=value; break;
-		case "actor": this.attributes[attr].push(value); break;
+		case "actor": this.attributes[attr].push(value.getname());break;
 		default: return console.log("error, attribute value must be a title or actor/s");
 		} 
 	},
@@ -25,33 +24,63 @@ Movie.prototype = {
 		switch (attr){											//this switch is used to control if the attribute to get is valid//
 		case "title": return(this.attributes[attr]); break;
 		case "actor": $.each(this.attributes.actor,function(index,value){
+			console.log(++index, value);
+		}); break;
+		default: return console.log("error, attribute value must be a title or actor/s");
+		} 
+	}
+}
+//-----------------------------class Movie defined as a Module------------------------
+MovieModule=(function(){  
+        this.title = '';
+        this.actor = [];
+	return{
+	play:function(){
+		$.publish("playing",[this.get("title")]);
+	},
+	stop:function(){
+		$.publish("stopped",[this.get("title")]);
+	},
+	set:function(attr,value){
+		switch (attr){											//this switch is used to control if the attribute to set is valid//
+		case "title": this[attr]=value; break;
+		case "actor": this[attr].push(value); break;
+		default: return console.log("error, attribute value must be a title or actor/s");
+		} 
+	},
+	get:function(attr){
+		switch (attr){											//this switch is used to control if the attribute to get is valid//
+		case "title": return(this[attr]); break;
+		case "actor": $.each(this[attr],function(index,value){
 			console.log(value);
 		}); break;
 		default: return console.log("error, attribute value must be a title or actor/s");
 		} 
 	}
 }
+})();
+
 
 //---------------------------------------------Listener-------------------------------------------//
-var MovieObserver={
-	listen:function(){
-		$.subscribe("playing",function(e,title){
-			console.log("playing "+title);
-		})
-		$.subscribe("stopped",function(e,title){
-			console.log("stopped "+title);
-		})
-	}
+MovieObserver=function(){
+	$.subscribe("playing",function(e,title){
+		console.log("playing "+title);
+	})
+	$.subscribe("stopped",function(e,title){
+		console.log("stopped "+title);
+	})
 }
 
 //-------------------------------------------Methods to extend class Movie----------------------------------//
 
-var downloadableMovie={
-	download:function(){
+downloadableMovie =function(){
+	Movie.call;
+},
+downloadableMovie.prototype=new Movie;
+downloadableMovie.prototype.download=function(){
 		console.log("you are trying to download "+this.get("title"));
-	}
 }
-
+//---------------------------MIXIN-----------------------------------------
 var social={
 	share:function(name){
 		console.log("shared "+this.get("title")+" with "+name);
@@ -60,13 +89,37 @@ var social={
 		console.log("you liked "+this.get("title")+" movie!")
 	}
 }
-
-$.extend(Movie.prototype,downloadableMovie);
 $.extend(Movie.prototype,social);
 
+//--------------------------Actor Class definition--------------------------
+Actor=function(){
+	this.name= "";
+};
+Actor.prototype={
+	setname:function(value){
+		this.name=value;
+	},
+	getname:function(){
+		return(this.name)
+	}
+};
  //--------------------------------------------------------------------------------------------------------//
- $(document).ready(function(){
- 	MovieObserver.listen();		//initialize the Listener
- 	first=new Movie;
- 	first.set("title","terminator");
- })
+new MovieObserver;		//initialize the Listener
+terminator=new Movie;
+terminator.set("title","terminator");
+terminator.play();
+terminator.stop();
+actor1=new Actor;
+actor1.setname("Arnold Schwarzenegger");
+actor2=new Actor;
+actor2.setname("Linda Hamilton");
+terminator.set("actor",actor1);
+terminator.set("actor",actor2);
+terminator.get("actor");
+terminator.share("thomas");
+terminator.like();
+alien=new downloadableMovie;
+alien.set("title","alien");
+alien.play();
+alien.download();
+//terminator.download(); //is not a downloadable movie, so this sentence would cause an error
