7e1d8f5b0482db7761f547bb10ce156181f44143
wenceslao01 <whoznogueira@gmail.com>
2014-11-09 04:02:22 -0300

    topic1 $.each implementation

--- 01-js-intro-and-jquery/scripts/fade.js
+++ 01-js-intro-and-jquery/scripts/fade.js
@@ -24,14 +24,14 @@ $(document).ready(function(){
             type: "get"
         })
         .success(function(data){
-            for(i=0; i<data.statuses.length;i++){
-                $("#tweetsect").append("<article><br/><strong>from user</strong>:"+ data.statuses[i].user.name);
-                $("#tweetsect").append("<strong>text:</strong>"+ data.statuses[i].text);
+            $.each(data.statuses,function(index,value){
+                $("#tweetsect").append("<article><br/><strong>from user</strong>:"+ value.user.name);
+                $("#tweetsect").append("<strong>text:</strong>"+ value.text);
                 $("#tweetsect").append("<br/>");
-                $("#tweetsect").append("<strong>created at:</strong>" + data.statuses[i].created_at);
+                $("#tweetsect").append("<strong>created at:</strong>" + value.created_at);
                 $("#tweetsect").append("<br/>");
-                $("#tweetsect").append("<strong>img url:</strong>" + data.statuses[i].user.profile_image_url + "</article>");
-            }
+                $("#tweetsect").append("<strong>img url:</strong>" + value.user.profile_image_url + "</article>");
+            })
         })
         .fail(function(){
                 alert('error');
